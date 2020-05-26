24bab1919ba347de54208cd51a716effb9a8838a
Liliana <liliana.cholody@gmail.com>
2020-05-27 00:17:34 +0200

    Add actual messages from table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Kacper", password: "password")
+User.create(username: "Jadzia", password: "password")
+User.create(username: "Iza", password: "password")
+User.create(username: "Blazej", password: "password")
+User.create(username: "Marek", password: "password") 
